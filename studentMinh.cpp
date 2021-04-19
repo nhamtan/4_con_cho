6740950a061fc105c2a61af8e9aa4e69f782e9db
vhyen <vanhoangyen0206@gmail.com>
2021-04-19 22:28:42 +0700

    Input Data 4 course

--- studentMinh.cpp
+++ studentMinh.cpp
@@ -1,26 +1,4 @@
-#include <bits/stdc++.h>
-
-using namespace std;
 
 
 
-void inputData4Course (Datacourse *&cs) // ham nay de tao data cho course
-{
 
-    Datacourse *cur = cs;
-    for (int i=0;i<5;i++)
-    {
-        cur=new Datacourse;
-        cur->id="CS161";
-        cur->name="Introduction to Computer Science 1";
-        cur->teacherName ="DinhBaTien";
-        cur->numOfCredits =4;
-        cur->maxSt=50;
-        cur->daySt[0].dayInWeek ="MON";
-        cur->daySt[0].time="S1";
-        cur->daySt[1].dayInWeek="FRI";
-        cur->daySt[1].time="S1";
-        cur->next=nullptr;
-        cur=cur->next;
-    }
-}
